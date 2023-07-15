f6e0b84c0ff254bd6e8ae1c3a080bb51bd84113f
Roger Killer <roger.killer@advery.ch>
2023-07-15 22:14:14 +0200

    📦 chore(package.json): add autoprefixer, postcss, prettier-plugin-tailwindcss, and tailwindcss dependencies 📝 docs(postcss.config.js): add postcss configuration file to enable Tailwind CSS and Autoprefixer 🔧 refactor(nx-welcome.tsx): update welcome message to include Tailwind CSS classes 🎨 style(styles.css): add Tailwind CSS directives for base, components, and utilities 📝 docs(tailwind.config.js): add tailwind configuration file to specify content and create glob patterns for dependencies

--- src/app/nx-welcome.tsx
+++ src/app/nx-welcome.tsx
@@ -418,7 +418,7 @@ export function NxWelcome({ title }: { title: string }) {
         <div className="container">
           <div id="welcome">
             <h1>
-              <span> Hello there, </span>
+              <span className="p-4 pt-4"> Hello there, I am using tailwind </span>
               Welcome {title} 👋
             </h1>
           </div>
