2cce9f2f65bc0c661dcff3180af728af3a7679c6
Roger Killer <roger.killer@advery.ch>
2023-07-16 21:54:10 +0200

    🔀 refactor(app.tsx): replace NxWelcome component with Layout component for improved code organization and consistency

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,12 +1,11 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.css';
-
-import NxWelcome from './nx-welcome';
+import { Layout } from './layout/layout';
 
 export function App() {
   return (
     <div>
-      <NxWelcome title="project-2" />
+      <Layout />
     </div>
   );
 }
