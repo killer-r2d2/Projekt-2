cf56006b9bf2a129c18a6502cac13265b898d7d9
Roger Killer <roger.killer@advery.ch>
2023-07-16 22:03:29 +0200

    ✨ feat(app.tsx): add ProductList component to the Layout component to display a list of products 📝 chore(product-list.module.css): create a new CSS module file for the ProductList component 🚧 test(product-list.spec.tsx): add a test for the ProductList component to ensure it renders successfully ✨ feat(product-list.tsx): create ProductList component to display a list of products with their details and images

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,11 +1,14 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.css';
 import { Layout } from './layout/layout';
+import { PoductList } from './product-list/product-list';
 
 export function App() {
   return (
     <div>
-      <Layout />
+      <Layout>
+        <PoductList />
+      </Layout>
     </div>
   );
 }
