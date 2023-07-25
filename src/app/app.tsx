7119e34a3f6848379c6f9ba78cd546a0522e97e9
Roger Killer <roger.killer@advery.ch>
2023-07-25 13:49:16 +0200

    🔧 chore(.gitignore): add .env to the list of ignored files to prevent sensitive information from being committed 📦 deps(package.json): add @supabase/supabase-js package as a dependency to enable integration with Supabase ✨ feat(app.tsx): add functionality to fetch and display a list of countries from Supabase using the @supabase/supabase-js package 📝 docs(supabaseClient.ts): create a new file supabaseClient.ts to handle the Supabase client configuration and initialization

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -6,8 +6,20 @@ import Projects from './pages/projects/projects';
 import Hooks from './pages/hooks/hooks';
 import Home from './pages/home/home';
 import Error404 from './pages/error404/error404';
+import { useEffect, useState } from "react";
+import { supabase } from '../supabaseClient'
 
 export function App() {
+  const [countries, setCountries] = useState([]);
+
+    useEffect(() => {
+      getCountries();
+    }, []);
+
+    async function getCountries() {
+      const { data } = await supabase.from("countries").select();
+      setCountries(data);
+    }
   return (
     <Layout>
       <Routes>
@@ -19,6 +31,11 @@ export function App() {
         <Route path="/hooks" element={<Hooks />} />
         <Route path="*" element={<Error404 />} />
       </Routes>
+      <ul>
+        {countries.map((country) => (
+          <li key={country.name}>{country.name}</li>
+        ))}
+      </ul>
     </Layout>
   );
 }
